d7b31c4697506fd9b02b1992a196e5567250c96e
Giovanni <giovanni.10leao@hotmail.com>
2025-04-04 10:10:38 -0300

    update

---
+++ 1007 - URI.py
@@ -0,0 +1,11 @@
+def calcDif(a, b, c, d):
+    
+    x = print("DIFERENCA =", (a*b)-(c*d));
+    return x;
+
+a = int(input());
+b = int(input());
+c = int(input());
+d = int(input());
+
+calcDif(a,b,c,d);
